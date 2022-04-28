35a8d5ebd3384ae19012591686325e2a4546e871
deepak <deepakdpyqaz@gmail.com>
2022-04-28 07:21:07 +0530

    cleanup fix

--- cleanup.py
+++ cleanup.py
@@ -1,4 +1,9 @@
 import subprocess
+import os
+if not os.path.exists("serving_model"):
+    os.mkdir("serving_model")
+if not os.path.exists("serving_model/dibetes_prediction_pipeline"):
+    os.mkdir("serving_model/dibetes_prediction_pipeline")
 subprocess.call('sudo cp -r /home/deepak/pipeline/serving_model/dibetes_prediction_pipeline/* /home/deepak/diabetes_prediction/serving_model/dibetes_prediction_pipeline/',shell=True)
 subprocess.call('sudo docker stop $(sudo docker ps -a -q)', shell=True)
 subprocess.call('sudo docker rm $(sudo docker ps -a -q)', shell=True)
