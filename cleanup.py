bdb9a1bb79127236d7eb48c5beb32a7d4142e0a2
deepak <deepakdpyqaz@gmail.com>
2022-04-28 07:25:50 +0530

    new flow

--- cleanup.py
+++ cleanup.py
@@ -1,9 +1,13 @@
 import subprocess
 import os
+print(os.getcwd())
 if not os.path.exists("serving_model"):
     os.mkdir("serving_model")
 if not os.path.exists("serving_model/dibetes_prediction_pipeline"):
     os.mkdir("serving_model/dibetes_prediction_pipeline")
+print(os.listdir())
+print(os.listdir("serving_model"))
+print(os.listdir("serving_model/dibetes_prediction_pipeline"))
 subprocess.call('sudo cp -r /home/deepak/pipeline/serving_model/dibetes_prediction_pipeline/* /home/deepak/diabetes_prediction/serving_model/dibetes_prediction_pipeline/',shell=True)
 subprocess.call('sudo docker stop $(sudo docker ps -a -q)', shell=True)
 subprocess.call('sudo docker rm $(sudo docker ps -a -q)', shell=True)
